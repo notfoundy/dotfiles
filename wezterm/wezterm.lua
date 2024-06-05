9b3249dabbf803d6261e2d1cfd22d06f72434951
Christophe Leconte <christophe@leconte.online>
2024-06-05 23:07:26 +0200

    Get the waifu back

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,11 +1,9 @@
 local wezterm = require("wezterm")
 return {
-	-- color_scheme = 'termnial.sexy',
 	color_scheme = "Catppuccin Mocha",
 	enable_tab_bar = false,
 	font_size = 16.0,
 	font = wezterm.font("JetBrains Mono Nerd Font"),
-	-- macos_window_background_blur = 40,
 	macos_window_background_blur = 30,
 	-- window_background_image = '/Users/omerhamerman/Downloads/3840x1080-Wallpaper-041.jpg',
 	-- window_background_image_hsb = {
@@ -13,11 +11,7 @@ return {
 	-- 	hue = 1.0,
 	-- 	saturation = 0.5,
 	-- },
-	-- window_background_opacity = 0.92,
-	window_background_opacity = 1.0,
-	-- window_background_opacity = 0.78,
-	-- window_background_opacity = 0.20,
-	window_decorations = "RESIZE",
+	window_background_opacity = 0.78,
 	keys = {
 		{
 			key = "f",
