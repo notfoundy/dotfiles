6c37f49da1025d141431c64f2c3dc516e7bd8d3d
Christophe Leconte <christophe@leconte.online>
2024-09-25 01:11:59 +0200

    replace theme with rose-pine

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,24 +1,12 @@
 local wezterm = require("wezterm")
+local theme = wezterm.plugin.require("https://github.com/neapsix/wezterm").main
+
 return {
-	color_scheme = "Catppuccin Mocha",
+	colors = theme.colors(),
 	enable_tab_bar = false,
 	font_size = 16.0,
 	font = wezterm.font("JetBrains Mono Nerd Font"),
-	macos_window_background_blur = 30,
-	-- window_background_image = '/Users/omerhamerman/Downloads/3840x1080-Wallpaper-041.jpg',
-	-- window_background_image_hsb = {
-	-- 	brightness = 0.01,
-	-- 	hue = 1.0,
-	-- 	saturation = 0.5,
-	-- },
 	window_background_opacity = 0.78,
-	keys = {
-		{
-			key = "f",
-			mods = "CTRL",
-			action = wezterm.action.ToggleFullScreen,
-		},
-	},
 	mouse_bindings = {
 		-- Ctrl-click will open the link under the mouse cursor
 		{
