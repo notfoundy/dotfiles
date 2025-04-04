1a10172cc307ee27c11cc46352574b3b020a4e3e
Christophe Leconte <christophe@leconte.online>
2025-04-04 13:58:10 +0200

    chore(bash): update aliases

--- roles/bash/files/bash/aliases/app.sh
+++ roles/bash/files/bash/aliases/app.sh
@@ -1,7 +1,8 @@
 # App but better...
-
 alias cl=clear
 alias cat=bat
 alias ll="eza -l --icons --git -a"
 alias lt="eza --tree --level=2 --long --icons --git"
 
+# System update
+alias update="sudo dnf update -y && flatpak update -y"
