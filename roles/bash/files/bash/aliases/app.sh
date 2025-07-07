1707fb5f250295fee5b9c874ca7ae52fb5ab31d4
Christophe Leconte <christophe@leconte.online>
2025-07-07 16:32:40 +0200

    chore: update vars

--- roles/bash/files/bash/aliases/app.sh
+++ roles/bash/files/bash/aliases/app.sh
@@ -3,6 +3,7 @@ alias cl=clear
 alias cat=bat
 alias ll="eza -l --icons --git -a"
 alias lt="eza --tree --level=2 --long --icons --git"
+alias vim=nvim
 
 # System update
 alias update="sudo dnf update -y && flatpak update -y"
--- roles/bash/files/bash/var.sh
+++ roles/bash/files/bash/var.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+export EDITOR="nvim"
+export VISUAL="$EDITOR"
+
 # NNN
 export NNN_PLUG='p:preview-tui'
 export NNN_FIFO=/tmp/nnn.fifo
