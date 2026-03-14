cd8eb9dfed4aab14754df79b922a659bc3c99a72
notfoundy <christophe@leconte.online>
2026-03-14 17:45:59 +0100

    feat: add quickshell

---
+++ roles/desktop-shell/files/themes/kanagawa.lua
@@ -0,0 +1,12 @@
+-- Kanagawa Colors for Quickshell (Statique)
+return {
+    background = "#1F1F28",
+    foreground = "#DCD7BA",
+    primary = "#7E9CD8",
+    secondary = "#957FB8",
+    accent = "#E82424",
+    success = "#76946A",
+    warning = "#FF9E3B",
+    error = "#C34043",
+    border = "#363646"
+}
