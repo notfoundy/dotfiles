4544b6bd11575eeced0459de9c54457b4b05fe27
notfoundy <christophe@leconte.online>
2026-05-20 22:03:42 +0200

    fix: update script path

--- roles/hyprdot/files/variables.lua
+++ roles/hyprdot/files/variables.lua
@@ -5,14 +5,14 @@
 hl.env("qsConfig", "ii")
 
 -- Apps
-terminal = "~/.config/hypr/hyprland/scripts/launch_first_available.sh 'ghostty' 'kitty -1' "
-fileManager = "~/.config/hypr/hyprland/scripts/launch_first_available.sh 'dolphin' 'nautilus' 'nemo' 'thunar' "
-browser = "~/.config/hypr/hyprland/scripts/launch_first_available.sh 'zen-browser' 'firefox' 'brave' 'chromium' 'opera'"
-codeEditor = "~/.config/hypr/hyprland/scripts/launch_first_available.sh 'command -v nvim && ghostty -1 nvim' 'zed'"
-officeSoftware = "~/.config/hypr/hyprland/scripts/launch_first_available.sh 'wps' 'libreoffice'"
-textEditor = "~/.config/hypr/hyprland/scripts/launch_first_available.sh 'kate' 'gnome-text-editor'"
-volumeMixer = "~/.config/hypr/hyprland/scripts/launch_first_available.sh 'pavucontrol-qt' 'pavucontrol'"
+terminal = "~/.config/hypr/scripts/launch_first_available.sh 'ghostty' 'kitty -1' "
+fileManager = "~/.config/hypr/scripts/launch_first_available.sh 'dolphin' 'nautilus' 'nemo' 'thunar' "
+browser = "~/.config/hypr/scripts/launch_first_available.sh 'zen-browser' 'firefox' 'brave' 'chromium' 'opera'"
+codeEditor = "~/.config/hypr/scripts/launch_first_available.sh 'command -v nvim && ghostty -e nvim' 'zed'"
+officeSoftware = "~/.config/hypr/scripts/launch_first_available.sh 'wps' 'libreoffice'"
+textEditor = "~/.config/hypr/scripts/launch_first_available.sh 'kate' 'gnome-text-editor'"
+volumeMixer = "~/.config/hypr/scripts/launch_first_available.sh 'pavucontrol-qt' 'pavucontrol'"
 settingsApp =
-	"XDG_CURRENT_DESKTOP=gnome ~/.config/hypr/hyprland/scripts/launch_first_available.sh 'qs -p ~/.config/quickshell/$qsConfig/settings.qml' 'systemsettings' 'gnome-control-center' 'better-control'"
+	"XDG_CURRENT_DESKTOP=gnome ~/.config/hypr/scripts/launch_first_available.sh 'qs -p ~/.config/quickshell/$qsConfig/settings.qml' 'systemsettings' 'gnome-control-center' 'better-control'"
 taskManager =
-	"~/.config/hypr/hyprland/scripts/launch_first_available.sh 'gnome-system-monitor' 'plasma-systemmonitor --page-name Processes' 'command -v btop && ghostty -1 bash -c btop'"
+	"~/.config/hypr/scripts/launch_first_available.sh 'gnome-system-monitor' 'plasma-systemmonitor --page-name Processes' 'command -v btop && ghostty -1 bash -c btop'"
